f400b81b95415a51afaba1f378e5a48c2f54162a
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-06-07 11:27:53 -0400

    2.html si ingreso 1 me devuelve 'uno'

--- src/literalizar-UI.test.js
+++ src/literalizar-UI.test.js
@@ -4,7 +4,7 @@ describe("literalizar html", () => {
     document.body.innerHTML = fs.readFileSync("index.html", "utf8");
     require("./presenter.js");
   });  
-  it("si ingreso 0 me devuelve 'cero' ", () => {
+  it("1.html si ingreso 0 me devuelve 'cero' ", () => {
     const num = document.querySelector("#numero");
     num.value=0;
     const boton = document.querySelector("#botonliteral");
@@ -14,4 +14,13 @@ describe("literalizar html", () => {
     expect(resultado.innerHTML).toEqual("cero");
   });
 
+  it("2.html si ingreso 1 me devuelve 'uno' ", () => {
+    const num = document.querySelector("#numero");
+    num.value=1;
+    const boton = document.querySelector("#botonliteral");
+    boton.click();
+
+    const resultado = document.querySelector("#resultado-div");
+    expect(resultado.innerHTML).toEqual("uno");
+  });
 });
